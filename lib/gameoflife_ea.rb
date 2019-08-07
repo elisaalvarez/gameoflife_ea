d917cead1ec5e27635251a2d3cc04052ca75b737
sp4ceunicorn <colorful_elisa@hotmail.com>
2019-08-07 09:28:16 -0500

    the gem creation for the game of life

---
+++ lib/gameoflife_ea.rb
@@ -0,0 +1,6 @@
+require "gameoflife_ea/version"
+
+module GameoflifeEa
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/gameoflife_ea/version.rb
@@ -0,0 +1,3 @@
+module GameoflifeEa
+  VERSION = "0.1.0"
+end
